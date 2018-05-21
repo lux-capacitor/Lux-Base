e15580c14923dc08b55eefb31740878583ca2dce
Brad McGlasson <brad.mcglasson@onestop.com>
2018-05-21 11:15:43 -0700

    CONFIG : Webpack + PostCSS : Updated webpack config and added in PostCSS config compiler

---
+++ config/config-envr.js
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ *
+ *						ENVIRONMENT VARIABLES
+ *     Can use these as a testing ground for flags until you setup the flags on
+ *		node server properly in the actual server architecture
+ *
+ *****************************************************************************/
+
+// EXPORTS
+module.exports = {}
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,104 @@
-var path = require('path');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 
-module.exports = {
-  devtool: 'eval',
+// ENVR : Are We Prod or Dev? (term "mode" comes from webpack 4)
+const envrConfig = {
+  mode: 'dev',
+  minify: false
+}
 
-  //Where is the dev server to run at?
+//PLUGINS : BASE + IMAGE HANDLER! (straight copy for now :p )
+let plugins = [
+  new ExtractTextPlugin( 'styles.css' ),
+  new CopyWebpackPlugin([
+    { 
+      from:'app/img', 
+      to:'img' 
+    } 
+  ])
+];
+
+
+//PLUGINS : PROD-ONLY : Minify Scripts
+if ( envrConfig.minify ) {
+  plugins = plugins.concat( [
+    // new CleanWebpackPlugin([ 'dist' ]), // Supposed to clean dist folder, doens't recompile sadly on start
+    new webpack.DefinePlugin({ //From Reactjs.org docs 
+      'process.env.NODE_ENV': JSON.stringify( 'production' )
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: envrConfig.minify,
+      compress: {
+        warnings: false
+      }
+    })
+  ] );
+}
+
+
+//CONFIG : Webpack Setup
+const config = {
+  // devtool: 'source-map', //Sourcemaps for dev builds
+
   entry: [
-    'webpack-dev-server/client?http://localhost:9001',
-    './app/index.jsx'
+    'babel-polyfill', //Add babel into bundle
+    './app/AppWrapper.jsx'
   ],
 
-  //Where do I output the bundled js? 
   output: {
-    path: path.join( __dirname, 'dist' ),
-    filename: 'bundle.js',
-    publicPath: '/app/'
+    path: path.resolve( __dirname, 'dist' ),
+    filename: 'bundle.js'
   },
 
-  //What types of files are we loading?
   module: {
-    loaders: [
-      {
+    rules: [
+      { //--JSX JUGGLER
         test: /\.jsx?$/,
         use: [ 'babel-loader' ],
         include: path.join( __dirname, 'app' )
       },
-      {
-        test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+
+      { //--FONT FLINGER
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
       },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          'url-loader?limit=10000',
-          'img-loader'
-        ]
+
+      { //--SCSS SMUSHER
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            { 
+              loader: 'css-loader', 
+              options: { 
+                importLoaders: 1,
+                minimize: envrConfig.minify
+              } 
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
       }
     ]
-  }
-}
+  },
+
+  plugins //Plugins Arr built up top
+};
+
+
+module.exports = config;
+
+
+
+
+
+
