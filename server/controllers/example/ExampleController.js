eac0e3fcb35ec84cd49d3a4b722213d1a2c89396
Brad McGlasson <brad.mcglasson@onestop.com>
2018-06-19 17:01:55 -0700

    SERVER : Methods : Following paradigm I've been using on the Merlin util, I split methods and controllers to better isolate individual purposes and follow with what seems more in convention of structure

--- server/controllers/example/ExampleController.js
+++ server/controllers/example/ExampleController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const methods = require( './example-methods' );
+const methods = require( '../../methods/example-methods' );
 
 /*****************************************************************************
  * EXAMPLE ROUTE : 
