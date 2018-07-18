085336e75c6e72b1739d8e5a0cf73477404cb010
Brad McGlasson <brad.mcglasson@onestop.com>
2018-07-18 15:08:10 -0700

    EXPRESS : Clarification notes on what each line is doing in the expressServer.js wrapper file

--- server/helpers/expressServer.js
+++ server/helpers/expressServer.js
@@ -4,12 +4,17 @@ const express = require( 'express' );
 module.exports = {
   app: function () {
     const app = express();
+
+    // CONFIG : Specify where our index file + static routes for assets exist
     const indexPath = path.join( __dirname, '../../index.html' );
     const publicPath = express.static( path.join( __dirname, '../../dist' ) );
 
+    // ROUTE : Set public assets to /dist folder, so any static asset 
+    //			access like:	'/dist/img/mypicture.jpg'
     app.use( '/dist', publicPath );
-    app.get( '/', function ( _, res ) { res.sendFile( indexPath ) });
 
+    // ROUTE : Set root route to the index html file
+    app.get( '/', function ( _, res ) { res.sendFile( indexPath ) });
     return app;
   }
 }
